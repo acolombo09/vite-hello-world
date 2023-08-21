2dcd58dfeb997cc0b535f59f02add9e69fabaa7a
acolombo09 <acolombo0911@gmail.com>
2023-08-21 20:18:40 +0200

    bonus_completed_(no bootstrap?)

--- vue-hello-world/src/components/Hero.vue
+++ vue-hello-world/src/components/Hero.vue
@@ -11,7 +11,25 @@ export default {
 </script>
 
 <template>
-  <h1>Hello from Hero Component, here below we will write our hero section</h1>
+  <div class="container-fluid">
+    <div class="row">
+      <div class="column">
+        <div class="p-5 text-center bg-image rounded-3" style="
+          background-image: url('https://mdbcdn.b-cdn.net/img/new/slides/041.webp');
+          height: 400px;">
+          <div class="mask" style="background-color: rgba(0, 0, 0, 0.6); height: 400px;">
+            <div class="d-flex justify-content-center align-items-center">
+              <div class="text-white">
+                <h1 class="my-5">Heading</h1>
+                <h4 class="my-5">Subheading</h4>
+                <button class="btn btn-outline-light btn-lg" href="#!">Call to action</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
